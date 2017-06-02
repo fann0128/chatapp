8999e29793a2353e6cb0a27c95b47838b6541764
fann0128 <yangfanballack@gmail.com>
2017-06-02 11:29:01 -0400

    chat app hahahha

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+'/index.html')
+})
+
+io.on('connection', function(socket){
+	socket.on('chat.message',function(message) {
+		io.emit('chat.message', message)
+	})
+})
+
+http.listen(3000,function(){
+  console.log('server started')
+})

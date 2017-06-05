0a743695cff176b7905ffedb5518c54b63a046e8
fann0128 <yangfanballack@gmail.com>
2017-06-05 14:34:37 -0400

    using compentent

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express = require('express')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
-app.use(express.static(__dirname));
+var mongoose = require('mongoose')
+
+app.use(express.static(__dirname))
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.sendFile(__dirname+'/index.html')
--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 var socket = io();
+
+Vue.component('message',{
+      props: ['message'],
+      template:`<li class="message">
+                  <div class="sender">
+                    <h3>{{message.sender}}</h3>
+                  </div>
+                  <div class="messageText">
+                    <h3>{{message.text}}</h3>
+                  </div>
+                </li>`,
+        mounted() {
+        },
+        methods:{
+        }
+    });
+
+Vue.component('chatbox',{
+      props: ['messages'],
+      template:`
+      <ul>
+        <message v-for="message in messages" :message="message"></message>
+      </ul>
+      `
+    });
+
 new Vue({
   el:'#chat-app',
   data:{
-    msg:'',
+    msgtext:'',
+    msg:{},
     msgHis:[],
   },
   created: function(){
@@ -12,8 +39,10 @@ new Vue({
   },
   methods:{
     send:function(){
+      this.msg = {sender:'fan',text:this.msgtext}
       socket.emit('chat.message', this.msg);
-      this.msg = '';
+      this.msgtext = '';
+      this.msg = {};
     }
   }
 })
